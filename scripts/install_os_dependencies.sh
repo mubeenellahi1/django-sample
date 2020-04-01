29abf21e25c7e6326e28d23484275506ed9ab1bc
mubeenellahi1 <ellahimubeen@gmail.com>
2020-04-01 20:24:06 +0500

    appspec added buildspec updated

---
+++ scripts/install_os_dependencies.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+sudo apt-get update
+sudo apt-get upgrade python3
+sudo apt-get install python3-venv
+sudo apt-get install build-essential libssl-dev libffi-dev python-dev
+sudo apt install python3-pip
---
+++ scripts/install_python_dependencies.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+source /www/venv/bin/activate
+python3 -m pip install -r /www/requirements.txt
